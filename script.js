f63fed7307daf87fb002bef9a23912b3de31fb9c
santosh8184 <santoshbellana999@gmail.com>
2021-07-31 13:03:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+    var minNum = [22,33,2,44,56]
+    var min = minNum[0];
+
+    for (var i=0; i<minNum.length; i++){
+
+           if (min > minNum[i]){
+               
+            min = minNum[i]
+
+           }
+
+
+    }
+    console.log(min)
+
+
+
+
+
+
